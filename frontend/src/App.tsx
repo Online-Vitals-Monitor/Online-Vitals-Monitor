76738017af965dc3ac7dc980a59356c1485dff55
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2025-10-23 15:24:51 -0700

    Added a navbar and changed font colors and background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MonitorView from './pages/monitorView';
 import ValuesView from './pages/controlVitalsView';
+import Navbar from './components/Navbar';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,9 +9,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <Router>
-          <nav>
-            <Link to="/">Monitor</Link> | <Link to="/values">Control Vitals</Link>
-          </nav>
+          <Navbar/>
           <Routes>
             <Route path="/" element={<MonitorView />} />
             <Route path="/values" element={<ValuesView />} />
