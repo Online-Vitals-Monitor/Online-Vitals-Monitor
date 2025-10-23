2b4c538ab3a8644dbd1b90176d1534958f8789ec
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2025-10-22 18:02:50 -0700

    added vital sliders

--- frontend/src/pages/monitorView.tsx
+++ frontend/src/pages/monitorView.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import VitalSlider from '../components/vitalSlider';
 
 const MonitorView: React.FC = () => {
-  return <h1>Monitor</h1>;
+  //use state to track slide variables
+  const [heartRate, setHeartRate] = useState<number>(65);
+  const [respRate, setRespRate] = useState<number>(10);
+
+  return (
+    <div className='slider-containers'>
+      <VitalSlider title="Heart Rate" step={1} min={30} max={180} currentVal={heartRate} onChange={setHeartRate} />
+      <VitalSlider title="Respiratory Rate" step={1} min={0} max={12} currentVal={respRate} onChange={setRespRate} />
+    </div>
+  );
 };
 
 export default MonitorView;
