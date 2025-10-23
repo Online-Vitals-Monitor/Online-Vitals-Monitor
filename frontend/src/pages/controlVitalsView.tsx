d2fd9237114753dea9f2e60fa6897690ddcd49d7
Madelyn Sadler <sadlerm@oregonstate.edu>
2025-10-23 11:00:34 -0700

    Swap monitor and vitals screen and add tab titles

--- frontend/src/pages/controlVitalsView.tsx
+++ frontend/src/pages/controlVitalsView.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const controlVitalsView: React.FC = () => {
-  return <h1>Controls Vitals</h1>;
+import VitalSlider from '../components/vitalSlider';
+
+const ControlVitalsView: React.FC = () => {
+  
+  //use state to track slide variables
+  const [heartRate, setHeartRate] = useState<number>(65);
+  const [respRate, setRespRate] = useState<number>(10);
+
+  useEffect(() => {
+    document.title = 'Controller'
+  }, []);
+
+  return (
+    <div className='slider-containers'>
+      <VitalSlider title="Heart Rate" step={1} min={30} max={180} currentVal={heartRate} onChange={setHeartRate} />
+      <VitalSlider title="Respiratory Rate" step={1} min={0} max={12} currentVal={respRate} onChange={setRespRate} />
+    </div>
+  );
 };
 
-export default controlVitalsView;
+export default ControlVitalsView;
--- frontend/src/pages/monitorView.tsx
+++ frontend/src/pages/monitorView.tsx
@@ -1,17 +1,15 @@
-import React, {useState} from 'react';
-import VitalSlider from '../components/vitalSlider';
+import React, {useState, useEffect} from 'react';
+
+
 
 const MonitorView: React.FC = () => {
-  //use state to track slide variables
-  const [heartRate, setHeartRate] = useState<number>(65);
-  const [respRate, setRespRate] = useState<number>(10);
 
-  return (
-    <div className='slider-containers'>
-      <VitalSlider title="Heart Rate" step={1} min={30} max={180} currentVal={heartRate} onChange={setHeartRate} />
-      <VitalSlider title="Respiratory Rate" step={1} min={0} max={12} currentVal={respRate} onChange={setRespRate} />
-    </div>
-  );
+  useEffect(() => {
+    document.title = 'Monitor'
+  }, []);
+
+  
+  return <h1>Monitor Vitals</h1>;
 };
 
 export default MonitorView;
