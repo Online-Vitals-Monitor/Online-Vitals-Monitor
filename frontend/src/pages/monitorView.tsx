f9d79681a267168b9449653f1560b0639b6c8fb9
Madelyn Sadler <sadlerm@oregonstate.edu>
2025-10-23 11:29:16 -0700

    Add static hr and rr value display

--- frontend/src/pages/monitorView.tsx
+++ frontend/src/pages/monitorView.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 
 
 
@@ -8,8 +8,28 @@ const MonitorView: React.FC = () => {
     document.title = 'Monitor'
   }, []);
 
-  
-  return <h1>Monitor Vitals</h1>;
+  // vital number display
+  const heartRate = 70
+  const respRate = 12
+
+  return (
+    <div className='container mt-4'>
+      <div className='card mb-3'>
+        <div className="card-body text-center">
+          <h5 className="card-title">Heart Rate bpm</h5>
+          <p className="card-text display-4">{heartRate}</p>
+        </div>
+      </div>
+
+      <div className="card mb-3">
+        <div className="card-body text-center">
+          <h5 className="card-title">Respiratory Rate</h5>
+          <p className="card-text display-4">{respRate}</p>
+        </div>
+      </div>
+
+    </div>
+  )
 };
 
 export default MonitorView;
