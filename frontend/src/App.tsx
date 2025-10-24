d7409604190b470ce917c757aaa88b90ec8b4f22
Bryce Khut <khut.bryce@gmail.com>
2025-10-24 01:49:21 -0700

    installed Bootstrap, added components folder in src, created header file with Header.tsx and css, made changes to app (deleted the header there)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MonitorView from './pages/monitorView';
-import ValuesView from './pages/controlVitalsView';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MonitorView from "./pages/monitorView";
+import ValuesView from "./pages/controlVitalsView";
+import "./App.css";
+import Header from "./components/header/Header";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <nav>
-            <Link to="/">Monitor</Link> | <Link to="/values">Control Vitals</Link>
-          </nav>
-          <Routes>
-            <Route path="/" element={<MonitorView />} />
-            <Route path="/values" element={<ValuesView />} />
-          </Routes>
-        </Router>
-      </header>
-    </div>
+    // <div className="App">
+    <>
+      <Header />
+      <Router>
+        <nav>
+          <Link to="/">Monitor</Link> | <Link to="/values">Control Vitals</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MonitorView />} />
+          <Route path="/values" element={<ValuesView />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
