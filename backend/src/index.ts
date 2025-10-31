b14de288f95dfea800ad1adb7172ce4de7307061
Jamie Liu <jliu01417@gmail.com>
2025-10-31 12:47:24 -0700

    started modifications to connect api to server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import vitalsRouter from './vitalsRouter';
+import pool from "./db";
 
 const app = express();
 const PORT = 4000;
@@ -8,8 +9,22 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
+interface users {
+  id: number;
+  name: string;
+}
 // routes
 app.use('/api/vitals', vitalsRouter);
+
+// app.get("/api/users", async (req: Request, res: Response) => {
+//   try {
+//     const result = await pool.query<users>("SELECT * FROM users");
+//     res.status(200).json(result.rows);  // <- typed correctly
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Server error");
+//   }
+// });
 
 // start server
 app.listen(PORT, () => {
