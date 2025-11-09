5d9373ddea5ac8719811e43bccdfcd87de519b70
Bryce Khut <khut.bryce@gmail.com>
2025-11-09 00:06:12 -0800

    vercel.json and overall vercel configs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,12 @@ app.use(express.json());
 // routes
 app.use('/api/vitals', vitalsRouter);
 
+
+// CODE COMMENTED OUT TO TEST VERCEL HOSTING
 // start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// Vercel provides serverless runtime, thus have to export the app
+// export default app;
