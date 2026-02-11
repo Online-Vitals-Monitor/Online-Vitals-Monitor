ffb2a0ad3c71aefaaa98c683d3d07187c313a4af
LyleMcCaffrey <mccaffrl@oregonstate.edu>
2026-02-10 23:19:13 -0800

    Added files for frontend session management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SessionProvider } from "./contexts/sessionContext";
 import MonitorView from "./pages/monitorView";
 import ValuesView from "./pages/controlVitalsView";
 import NavbarNew from "./components/navbar/NavbarNew";
 import Homepage from "./pages/homepage/Homepage";
+import RequireSession from './components/RequireSession';
 import { VitalsProvider } from "./contexts/vitalsContext";
 import "./App.css";
 
@@ -14,17 +16,21 @@ const App = () => {
       <div className="app">
         <div className="app-header">
           <VitalsProvider>
-            <Router>
-              <NavbarNew
-                isSessionActive={sessionActive}
-                setIsSessionActive={setSessionActive}
-              />
-              <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/monitor" element={<MonitorView />} />
-                <Route path="/values" element={<ValuesView />} />
-              </Routes>
-            </Router>
+            <SessionProvider>
+              <Router>
+                <NavbarNew
+                  isSessionActive={sessionActive}
+                  setIsSessionActive={setSessionActive}
+                />
+                <Routes>
+                  <Route path="/" element={<Homepage />} />
+                  <Route element={<RequireSession />}>
+                    <Route path="/monitor" element={<MonitorView />} />
+                    <Route path="/values" element={<ValuesView />} />
+                  </Route>
+                </Routes>
+              </Router>
+            </SessionProvider>
           </VitalsProvider>
         </div>
       </div>
---
+++ frontend/src/components/RequireSession.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useSession } from "../contexts/sessionContext";
+
+const RequireSession: React.FC = () => {
+  const { isConnected } = useSession();
+  const location = useLocation();
+
+  // Currently always passes for easier dev work
+  if (!isConnected) {
+    return <Navigate to="/session" replace state={{ from: location }} />;
+  }
+
+  return <Outlet />;
+};
+
+export default RequireSession;
